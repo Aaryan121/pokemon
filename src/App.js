272ff29e11b20065cf99e2ed92f1d8f2531fad66
Aaryan121 <aaryan.cs01@gmail.com>
2022-11-14 22:49:30 +0545

    single page designed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./Components/Navbar/Navbar";
 import MainPage from "./pages/mainPage/MainPage";
 import "./App.css"
+import SinglePoke from "./pages/singlePoke/SinglePoke";
 
 
 function App() {
   return (
   <>  
     <Navbar />
-    <MainPage /></>
+    <SinglePoke />
+  </>
   );
 }
 
--- src/pages/singlePoke/SinglePoke.js
+++ src/pages/singlePoke/SinglePoke.js
@@ -1,8 +1,45 @@
+import { useSelector } from "react-redux"
 import "./SinglePoke.css"
 
 const SinglePoke = () => {
-  return (
-    <div>SinglePoke</div>
+
+    const pokeData = useSelector((state) => state.pokemon)
+  
+    return (
+    <div className="singlePoke">
+        <div className="imageWrapper">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="" />
+        </div>
+        <div className="textWrapper">
+        <div className="name">Bulbosor</div>
+        <div className="desc1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, natus?
+        </div>
+
+        <div className="statContainer">
+            <ul>
+                <li>Height: 66</li>
+                <li>Weight: 49</li>
+                <li>Gender: Male</li>
+                <li>Category: Flame</li>
+                <li>Abilities: Blaze</li>
+            </ul>
+        </div>
+
+        <div className="types1">
+            fire
+        </div>
+
+        <div className="weakness">
+            <ul>
+                <li>Water</li>
+                <li>Ground</li>
+                <li>Rock</li>
+            </ul>
+        </div>
+        </div>
+        
+    </div>
   )
 }
 
